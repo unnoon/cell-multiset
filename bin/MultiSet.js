848fbe0c16f1e61e30d2d665c6c8afb7bd8c8729
unnoon <r.geertzema@gmail.com>
2016-08-24 17:01:56 -0600

    0.0.2

--- bin/MultiSet.js
+++ bin/MultiSet.js
@@ -25,7 +25,7 @@ const properties = {
     "static info": {
         "name"       : "cell-multiset",
         "description": "Fast JS MultiSet implementation.",
-        "version"    : "0.0.1",
+        "version"    : "0.0.2",
         "url"        : "https://github.com/unnoon/cell-multiset"
     },
     /**
--- bin/es5/MultiSet.js
+++ bin/es5/MultiSet.js
@@ -39,7 +39,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
         "static info": {
             "name": "cell-multiset",
             "description": "Fast JS MultiSet implementation.",
-            "version": "0.0.1",
+            "version": "0.0.2",
             "url": "https://github.com/unnoon/cell-multiset"
         },
         /**
